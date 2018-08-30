4b8dda59e3a0182ce013334e1c6e0655a8b8c898
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2018-08-30 10:53:48 -0300

    Adicionando exemplo com funcoes simples e de alta ordem

---
+++ DemoFSharp/DemoFSharp/Funcoes.fs
@@ -0,0 +1,26 @@
+﻿module Funcoes
+
+open System
+
+let ordenarCrescente listaNomes=
+    List.sort listaNomes
+
+let ordenarDerescente listaNomes =
+    List.sortDescending listaNomes
+
+let converterMaiusculas listaNomes =
+    listaNomes 
+    |> List.map (fun nome -> nome.ToString().ToUpper())
+
+let converterMinusculas listaNomes =
+    listaNomes 
+    |> List.map (fun nome -> nome.ToString().ToLower())
+
+let imprimirNomes listaNomes =
+    listaNomes
+    |> List.iter (fun nome -> printfn "%s" nome)
+
+let imprimirNomesAlteracaoCaixa listaNomes funcaoCaixa funcaoImprimir =
+    listaNomes
+    |> funcaoCaixa
+    |> funcaoImprimir
---
+++ DemoFSharp/DemoFSharp/Program.fs
@@ -0,0 +1,46 @@
+﻿module Main
+
+open System
+open Funcoes
+
+[<EntryPoint>]
+let main argv =
+    printfn "Ordena items, em maiúsculas"
+    
+    let mutable listaNomes : string list = []
+
+    let mutable nome = Console.ReadLine()
+
+    while nome <> "" do
+        listaNomes <- List.append listaNomes [nome]
+        nome <- Console.ReadLine()
+
+    printfn "Lista simples"
+    imprimirNomes listaNomes
+
+    printfn ""
+    Console.ReadKey() |> ignore
+
+    printfn "Lista com funcoes encadeadas"
+    listaNomes
+    |> ordenarDerescente
+    |> converterMaiusculas
+    |> imprimirNomes
+
+    printfn ""
+    Console.ReadKey() |> ignore
+
+
+    printfn "Lista com funcoes de alta ordem maiusculas"
+    imprimirNomesAlteracaoCaixa listaNomes converterMaiusculas imprimirNomes
+
+    printfn ""
+    Console.ReadKey() |> ignore
+
+    printfn "Lista com funcoes de alta ordem decrescente"
+    imprimirNomesAlteracaoCaixa listaNomes ordenarDerescente imprimirNomes
+
+    printfn ""
+    Console.ReadKey() |> ignore
+
+    0 // return an integer exit code
